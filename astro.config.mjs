9f0ed743ada4b142d0c8a2e4a29ff7b9c3ffcc62
Jonny Burch <jonny@progression.co>
2025-01-22 17:38:38 +0000

    change settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: "https://toy.studio",
-  output: "server",
   adapter: cloudflare({
     platformProxy: {
       enabled: true
