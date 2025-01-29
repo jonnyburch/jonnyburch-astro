f4e5c450a354f4fc5a6679915061f32efd63a4af
Jonny Burch <jonny@progression.co>
2025-01-29 17:19:52 +0000

    fix xml links

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -3,16 +3,18 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
   const posts = await getCollection('posts');
-  const activePosts = posts.filter(post => post.data.draft !== true);
+  const activePosts = posts.filter(post => post.data.draft !== true)
+    .sort((a, b) => b.data.date - a.data.date);
   return rss({
     title: 'Jonny Burch',
     description: 'Designer, Product Wonk, Founder, etc.',
     site: context.site,
+    trailingSlash: false,
     items: activePosts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/${post.id}/`
+      link: post.id.split('.')[0]
     })),
     customData: `<language>en-uk</language>`,
   });
