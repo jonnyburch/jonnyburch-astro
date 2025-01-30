99f6d55982b9f2bf1999a0cadd1f8d2a8d6c81b1
Jonny Burch <jonny@progression.co>
2025-01-30 09:53:17 +0000

    adding full post text

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,5 +1,8 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('posts');
@@ -11,10 +14,13 @@ export async function GET(context) {
     site: context.site,
     trailingSlash: false,
     items: activePosts.map((post) => ({
+      link: `/${post.id.split('.')[0]}/`,
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/${post.id.split('.')[0]}/`
+      content: sanitizeHtml(parser.render(post.body || ''), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
     customData: `<language>en-uk</language>`,
   });
